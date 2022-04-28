5a907c3846537af03fd9d380792de0bf990a2917
alewter12 <89615951+alewter12@users.noreply.github.com>
2022-04-28 13:10:01 -0500

    Update item.py
    The previous class did not work. This is a correction.

--- item.py
+++ item.py
@@ -1,66 +1,27 @@
 
-library = dict(
+class Item: 
+
+    def __init__(self, title, author, ISBN, genre, year, stock, price):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.genre = genre
+        self.year = year
+        self.stock = stock
+        self.price = price
+
+    def decreaseStock(self, quan):
+        self.stock = self.stock - quan
     
-    {"ISBN":"2543","Title":"Harry Potter: Sorcer Stone", "Author": "J.K. Rowling","Genre":"fantasy", "Year":"1998"},
-    {"ISBN":"1654","Title":"The Shining","Author":"Stephen King", "Genre":"horror","Year":"1977"},
-    {"ISBN":"7635","Title":"Gone Girl","Author":"Gillian Flynn","Genre":"mystery","Year":"2012"},
-    {"ISBN":"5634","Title":"Better Than Before","Author":"Gretchen Rubin","Genre":"wellness","Year":"2015"},
-    {"ISBN":"8967","Title":"1491","Author":"Charles C. Mann","Genre":"history","Year":"2005"},
-    {"ISBN":"8536","Title":"To Kill A Mockingbird","Author":"Harper Lee", "Genre":"Novel", "Year":"1960"},
-    {"ISBN":"3564","Title":"Berserk","Author":"Hakusensha","Genre":"Manga","Year":"1990"}
+    def getStock(self):
+        return self.stock
 
-)
+    def getPrice(self):
+        return self.price
 
-class Item():
+    def getTitle(self):
+        return self.title
 
-     def viewCategory(Genre, Author, Title):
-      history = str()
-      horror = str()
-      fantasy = str()
-      mystery = str ()
-      wellness =str()
-      novel=str()
-      manga=str()
-      Genre= tuple(str(history,horror, fantasy, mystery, wellness, novel, manga))
-      Author = str()
-      Title=str()
-      
-      input ( "Please search a Title, Author, or Genre : ")
-      
-      if (Genre): 
-            input("what is the Genre of the book you are looking for ?:")
-            print(library["Genre", "Title"])
-      
-      elif (Author):
-             input("who is the author you are looking for ?")
-             print(library["Author","Title"])
-     
-      elif (Title):
-             input(" what is the title of the book you are looking for ?")
-             print(library["Title"])
-
-   
-def decreaseStock(int):
-     Stock=int()
-     Price= int()
-     quanity= int(50)
-     itemsold=int()
-    
-     for i in range (quanity):
-         stock = print(" number of items in stock",(i-1), end = ":")
-
-    
-def book(self):
-      ISBN = int()
-      Title= str()
-      Author=str()
-      Genre=str()
-      Year=int()
-      Stock=int()
-      Price=float()
-      
-      book = tuple(ISBN,Title, Author,Genre, Year, Stock)
-      
-      return book(ISBN, Title, Author,Genre, Year, Stock , Price). format(self.ISBN, self.Title,self.Author, self.Genre, self. Year, self.Stock, self.Price)
-()
-
+    def display(self):
+        print(self.title + " " + self.author + " " + self.ISBN + " " + self.genre + " " + self.year)
+        print("In stock: " + self.stock + "   $" + self.price)
