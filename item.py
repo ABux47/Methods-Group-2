06426112e6758aaa83fccc9d129ee5373a2c4a09
alewter12 <89615951+alewter12@users.noreply.github.com>
2022-04-28 13:33:20 -0500

    Update item.py
    Adding file storage

--- item.py
+++ item.py
@@ -1,14 +1,13 @@
-
 class Item: 
 
-    def __init__(self, title, author, ISBN, genre, year, stock, price):
-        self.title = title
-        self.author = author
-        self.ISBN = ISBN
-        self.genre = genre
-        self.year = year
-        self.stock = stock
-        self.price = price
+    def __init__(self):
+        self.title = ""
+        self.author = ""
+        self.ISBN = ""
+        self.genre = ""
+        self.year = ""
+        self.stock = 0
+        self.price = 0
 
     def decreaseStock(self, quan):
         self.stock = self.stock - quan
@@ -25,3 +24,31 @@ class Item:
     def display(self):
         print(self.title + " " + self.author + " " + self.ISBN + " " + self.genre + " " + self.year)
         print("In stock: " + self.stock + "   $" + self.price)
+
+    def export(self):    
+        file = open("inventory.txt", "a")
+        file.write(self.title + "\n")
+        file.write(self.author + "\n")
+        file.write(self.ISBN + "\n")
+        file.write(self.genre + "\n")
+        file.write(self.year + "\n")
+        file.write(str(self.stock) + "\n")
+        file.write(str(self.price) + "\n")
+
+    def getFromFile(self):
+        file = open("inventory.txt", "r")
+        self.title = file.readline()
+        self.title.replace('\n', '')
+        self.author = file.readline()
+        self.author.replace('\n', '')
+        self.ISBN = file.readline()
+        self.ISBN.replace('\n', '')
+        self.genre = file.readline()
+        self.genre.replace('\n', '')
+        self.year = file.readline()
+        self.year.replace('\n', '')
+        self.stock = file.readline()
+        self.stock.replace('\n', '')
+        self.price = file.readline()
+        self.price.replace('\n', '')
+ 
