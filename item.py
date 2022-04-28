39c46ec19f54b1a0a357a91483d176da6cb55a84
alewter12 <89615951+alewter12@users.noreply.github.com>
2022-04-28 16:29:16 -0500

    Update item.py
    Little tweaks to make it mesh with the other classes

--- item.py
+++ item.py
@@ -9,6 +9,15 @@ class Item:
         self.stock = 0
         self.price = 0
 
+    def create(self, title, author, ISBN, genre, year, stock, price):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.genre = genre
+        self.year = year
+        self.stock = stock
+        self.price = price
+
     def decreaseStock(self, quan):
         self.stock = self.stock - quan
     
@@ -18,11 +27,14 @@ class Item:
     def getPrice(self):
         return self.price
 
+    def getGenre(self):
+        return self.genre
+
     def getTitle(self):
         return self.title
 
     def display(self):
-        print(self.title + " " + self.author + " " + self.ISBN + " " + self.genre + " " + self.year)
+        print(self.title + "  " + self.author + "  " + self.ISBN + "  " + self.genre + "  " + self.year)
         print("In stock: " + self.stock + "   $" + self.price)
 
     def export(self):    
@@ -35,20 +47,18 @@ class Item:
         file.write(str(self.stock) + "\n")
         file.write(str(self.price) + "\n")
 
-    def getFromFile(self):
-        file = open("inventory.txt", "r")
+    def getFromFile(self, file):
         self.title = file.readline()
-        self.title.replace('\n', '')
+        self.title = self.title.strip()
         self.author = file.readline()
-        self.author.replace('\n', '')
+        self.author = self.author.strip()
         self.ISBN = file.readline()
-        self.ISBN.replace('\n', '')
+        self.ISBN = self.ISBN.strip()
         self.genre = file.readline()
-        self.genre.replace('\n', '')
+        self.genre = self.genre.strip()
         self.year = file.readline()
-        self.year.replace('\n', '')
+        self.year = self.year.strip()
         self.stock = file.readline()
-        self.stock.replace('\n', '')
+        self.stock = self.stock.strip()
         self.price = file.readline()
-        self.price.replace('\n', '')
- 
+        self.price = self.price.strip()
