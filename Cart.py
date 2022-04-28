30c2d2362b41f24f24581e7412c893bb3a97c691
kaegan03 <104153953+kaegan03@users.noreply.github.com>
2022-04-28 17:01:13 -0500

    Update Cart.py

--- Cart.py
+++ Cart.py
@@ -2,52 +2,57 @@ from Item import Item
 class Cart:
     def __init__(self):
         self.itemList = {}
-        self.total= 0
+        
 
-    def ViewItems(self):
-        print("Items in stock: ")
-        for i in self.itemList:
-            print(i.name, i.quantity, '{0:.2f}'.format(i.price))
+    def ViewItems(self,item):
+        self.item = item
+        Item.display(item)
 
-    def AddItem(self, item, quantity=1):
-        if quantity > 0:
+
+    def AddItem(self, item, stock=1):
+        self.item = stock
+        if stock > 0:
             if item in self.itemList:
-                self.itemList[item] += quantity
-            elif quantity > item.quantity:
+                task = input("That item already exists in the cart! Would you like to add more? (Y/N): ")
+                if task == "Y":
+                    more = int(input("How many more would you like to add? "))
+                    self.itemList[item] = stock + more
+                else:
+                    return None
+                self.itemList[item] = stock
+            elif stock > Item.getStock(item):
                 print("Quantity chosen is greater than what's in stock")
             else:
-                self.itemList[item] = quantity
-        self.total=0
+                self.itemList[item] = stock
+        Item.display(item)
+        totalCost = 0
         for item in self.itemList:
-            quantity = self.itemList[item]
-            cost = quantity * item.price
-            print("Title: " + item.name + " Quantity: " + str(quantity) + " Cost: " + '{0:.2f}'.format(cost))
-            self.total += cost
-        print("Your current total is $" + '{0:.2f}'.format(self.total) + '\n')
+            stock = self.itemList[item]
+            cost = stock * item.price
+            print("Title: " + item.title + " Quantity: " + str(stock) + " Cost: " + '{0:.2f}'.format(cost))
+            totalCost += cost
+        print("Your current total is $" + '{0:.2f}'.format(totalCost) + '\n')
 
-    def DeleteItem(self, item, quantity):
-        if quantity <= 0:
+    def DeleteItem(self, item, stock=1):
+        if stock <= 0:
             self.itemList.pop(item, None)
         else:
             if item in self.itemList:
-                if quantity < self.itemList[item]:
-                    self.itemList[item] -= quantity
+                if item in self.itemList:
+                    self.itemList[item] -= stock
+                    tmp = self.itemList[item]
+                    print("The quantity of " + item.title + " has been decreased by " + str(stock) + " leaving " + str(tmp) + " book(s) of this title in the cart" )
                 else:
                     self.itemList.pop(item, None)
     
-    def PlaceOrder(self):
-        self.total = 0
+    def PlaceOrder(self,stock=1):
+        totalCost = 0
+        self.item = stock
         for item in self.itemList:
             quantity = self.itemList[item]
             cost = quantity * item.price
-            print("Title: " + item.name + " Quantity: " + str(quantity) + " Cost: " + '{0:.2f}'.format(cost))
-            self.total += cost
-            item.quantity -= quantity
-        print("Grand total for this order is $" + '{0:.2f}'.format(self.total) + '\n')
-
-    def AddDuplicateItem(self, item, quantity):
-        if quantity > 0:
-            self.itemList[item] = quantity
-        else:
-            self.DeleteItem(item)
-
+            tmp = self.itemList[item]
+            print("Title: " + item.title + " Quantity: " + str(tmp) + " Cost: " + '{0:.2f}'.format(cost))
+            totalCost += cost
+            Item.decreaseStock(item,tmp)
+        print("Grand total for this order is $" + '{0:.2f}'.format(totalCost) + '\n')
